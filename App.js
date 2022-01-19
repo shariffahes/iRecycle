bae7d5a7a581f1c1204100195b170dddd0c1dbbd
KaremAridi <80266460+KaremAridi@users.noreply.github.com>
2022-01-19 13:48:55 +0200

    setting the medium

--- App.js
+++ App.js
@@ -1,18 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import MainNavigator from './src/Navigation/MainNavigator';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import MainNavigator from "./src/Navigation/MainNavigator";
+import AppLoading from "expo-app-loading";
+import { useState } from "react";
+
+// Fonts
+const fetchFonts = () => {
+  return Font.loadAsync({
+    roboto: require("./assets/fonts/Roboto-Regular.ttf"),
+    "roboto-bold": require("./assets/fonts/Roboto-Bold.ttf"),
+  });
+};
 
 export default function App() {
-  return (
-      <MainNavigator/>
-  );
+  const [loadedFont, setLoadedFont] = useState(false);
+
+  // if (!loadedFont) {
+  //   return (
+  //     <AppLoading
+  //       startAsync={fetchFonts}
+  //       onFinish={() => setDataLoaded(true)}
+  //       onError={(err) => console.log(err)}
+  //     />
+  //   );
+  // }
+  return <MainNavigator />;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
