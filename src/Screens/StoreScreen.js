8ef2ccef9d0a9c26bbfd2d9f438767334815e048
KaremAridi <80266460+KaremAridi@users.noreply.github.com>
2022-02-08 23:09:10 +0200

    added banner card

--- src/Screens/StoreScreen.js
+++ src/Screens/StoreScreen.js
@@ -1,13 +1,30 @@
 import React from "react";
 import { FlatList, StyleSheet, Text, View } from "react-native";
+import BannerCard from "../Components/BannerCard";
 import InfoCard from "../Components/InfoCard";
 
 const StoreScreen = () => {
   return (
     <View style={styles.screen}>
       <View style={styles.list}>
+        <BannerCard
+          title={"Hello World"}
+          coins={250}
+          discount={50}
+          website={"helloworld.com"}
+          image="../../assets/icons/purseIcon.jpg"
+          // image sending not working
+        ></BannerCard>
+
         {/* <FlatList style={styles.list}> */}
-        <InfoCard title={"Hello World"} website={"helloworld.com"} coins={200} discount={20} image={"../../assets/icons/vendingIcon.png"}></InfoCard>
+        
+        <InfoCard
+          title={"Hello World"}
+          website={"helloworld.com"}
+          coins={200}
+          discount={20}
+          image={"../../assets/icons/vendingIcon.png"}
+        ></InfoCard>
       </View>
     </View>
   );
@@ -16,7 +33,7 @@ const StoreScreen = () => {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    backgroundColor:"#F5F5F5"
+    backgroundColor: "#F5F5F5",
   },
   list: {
     marginVertical: 10,
