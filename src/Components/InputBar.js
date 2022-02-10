703a4fb5b3ae61ff33941ef69e4696d9990d79f0
KaremAridi <80266460+KaremAridi@users.noreply.github.com>
2022-02-10 14:37:21 +0200

    auth screen ui input bars done

---
+++ src/Components/InputBar.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { View, StyleSheet, TextInput } from "react-native";
+import { Ionicons, Entypo } from "@expo/vector-icons";
+
+const InputBar = ({
+  keyboardType,
+  placeholder,
+  onChangeTextHandler,
+  icon,
+  secureTextBool,
+  ...rest
+}) => {
+  return (
+    <View style={styles.bar}>
+      <Ionicons name={icon} style={styles.icon} />
+      <TextInput
+        keyboardType={keyboardType}
+        onChangeText={onChangeTextHandler}
+        placeholder={placeholder}
+        style={styles.input}
+        secureTextEntry={secureTextBool}
+      ></TextInput>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  bar: {
+    flexDirection: "row",
+    padding: 10,
+    width: 342,
+    height: 52,
+    backgroundColor: "white",
+    shadowColor: "black",
+    shadowOpacity: 0.1,
+    shadowOffset: { width: 2, height: 2 },
+    borderRadius: 5,
+    alignItems: "center",
+    marginVertical:10
+  },
+  input: {
+    marginLeft: 10,
+    width: "80%",
+    borderBottomColor: "grey",
+    borderBottomWidth: 0.2,
+  },
+  icon: {
+    fontSize: 30,
+  },
+});
+
+export default InputBar;
--- src/Screens/AuthScreen.js
+++ src/Screens/AuthScreen.js
@@ -1,13 +1,44 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, TextInput, View, Text } from "react-native";
+import InputBar from "../Components/InputBar";
 
 const AuthScreen = () => {
-    return (
-        <View>
-            <Text>AuthScreen</Text>
-        </View>
-    );
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const setUsernameHandler = (selectedUsername) => {
+    setUsername(selectedUsername);
+  };
+
+  const setPasswordHandler = (selectedPassword) => {
+    setPassword(selectedPassword);
+  };
+
+  return (
+    <View style={styles.screen}>
+      <InputBar
+        icon="mail-outline"
+        keyboardType="email-address"
+        placeholder="Email"
+        onChangeTextHandler={setUsernameHandler}
+      ></InputBar>
+
+      <InputBar
+        icon="lock-closed-outline"
+        keyboardType="default"
+        placeholder="Password"
+        secureTextBool={true}
+        onChangeTextHandler={setPasswordHandler}
+      ></InputBar>
+    </View>
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "#F5F5F5",
+  },
+});
 export default AuthScreen;
