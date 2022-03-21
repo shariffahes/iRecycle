414f823a093eb323431dd0beed0e17091bb9b00f
KaremAridi <80266460+KaremAridi@users.noreply.github.com>
2022-03-21 23:24:03 +0200

    user page ui done

--- src/Screens/ProfileScreen.js
+++ src/Screens/ProfileScreen.js
@@ -23,52 +23,60 @@ const ProfileScreen = () => {
     },
   ];
   return (
-    <View style={styles.screen}>
-      <ProfileHeader name={"Sharif Fahes"} points={"1000"} />
-      <View style={styles.container}>
-        <CustomText fontSize={24} bold={true} color={"black"}>
-          Coupons
-        </CustomText>
-        <ScrollView horizontal={true}>
-          {arr.map((coupon) => {
-            return (
-              <View>
-                <Coupon
-                  title={coupon.title}
-                  coins={coupon.coins}
-                  expiryDate={coupon.expiryDate}
-                  discount={coupon.discount}
-                  image={coupon.image}
-                />
-              </View>
-            );
-          })}
-        </ScrollView>
-        <CustomText fontSize={24} bold={true} color={"black"} style={{marginTop:10}}>
-          Activities
-        </CustomText>
-        <ScrollView horizontal={true}>
-          <View>
-            <Coupon
-              title={"Recycle Now!"}
-              image={
-                "https://img.freepik.com/free-vector/flat-design-ecology-concept-with-natural-elements_23-2148219476.jpg?t=st=1647895379~exp=1647895979~hmac=352b7c86229d2acb244df05669459ae7231b04efdc7435fda5fd06e37b0deff5&w=740"
-              }
-              style={{ height: 300, width: 200 }}
-            />
-          </View>
-          <View>
-            <Coupon
-              title={"Volunteer With Us!"}
-              image={
-                "https://img.freepik.com/free-vector/flat-design-ecology-concept-with-natural-elements_23-2148220332.jpg?t=st=1647896607~exp=1647897207~hmac=78ef9df72ebb24d83f1d9a5328beb990e6b5851a4fe40c36bd559b518e2e648e&w=740"
-              }
-              style={{ height: 300, width: 200 }}
-            />
-          </View>
-        </ScrollView>
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <View style={styles.screen}>
+        <ProfileHeader name={"Sharif Fahes"} points={"1000"} />
+        <View style={styles.container}>
+          <CustomText fontSize={24} bold={true} color={"black"}>
+            Coupons
+          </CustomText>
+          <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+            {arr.map((coupon) => {
+              return (
+                <View>
+                  <Coupon
+                    title={coupon.title}
+                    coins={coupon.coins}
+                    expiryDate={coupon.expiryDate}
+                    discount={coupon.discount}
+                    image={coupon.image}
+                  />
+                </View>
+              );
+            })}
+          </ScrollView>
+          <CustomText
+            fontSize={24}
+            bold={true}
+            color={"black"}
+            style={{ marginTop: 30 }}
+            showsHorizontalScrollIndicator={false}
+          >
+            Activities
+          </CustomText>
+          <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} style={{marginBottom:10}}>
+            <View>
+              <Coupon
+                title={"Recycle Now!"}
+                image={
+                  "https://img.freepik.com/free-vector/flat-design-ecology-concept-with-natural-elements_23-2148219476.jpg?t=st=1647895379~exp=1647895979~hmac=352b7c86229d2acb244df05669459ae7231b04efdc7435fda5fd06e37b0deff5&w=740"
+                }
+                style={{ height: 300, width: 200 }}
+              />
+            </View>
+            <View>
+              <Coupon
+                title={"Volunteer With Us!"}
+                image={
+                  "https://img.freepik.com/free-vector/flat-design-ecology-concept-with-natural-elements_23-2148220332.jpg?t=st=1647896607~exp=1647897207~hmac=78ef9df72ebb24d83f1d9a5328beb990e6b5851a4fe40c36bd559b518e2e648e&w=740"
+                }
+                style={{ height: 300, width: 200 }}
+              />
+            </View>
+          </ScrollView>
+        </View>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
