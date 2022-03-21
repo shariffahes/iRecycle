b4b5be633b679550cf64cf460e7019080e96b1dd
KaremAridi <80266460+KaremAridi@users.noreply.github.com>
2022-03-21 13:38:38 +0200

    navigation for profile worked

--- src/Navigation/MenuStack.js
+++ src/Navigation/MenuStack.js
@@ -1,6 +1,7 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
 import MenuScreen from "../Screens/MenuScreen";
+import ProfileScreen from "../Screens/ProfileScreen"
 import { renderHeaderOptions } from "../constants/CustomFts";
 
 const OptionsStack = createNativeStackNavigator();
@@ -9,6 +10,7 @@ const MenuStack = () => {
   return (
     <OptionsStack.Navigator>
       <OptionsStack.Screen name="MainMenu" component={MenuScreen} options={renderHeaderOptions}/>
+      <OptionsStack.Screen name="Profile" component={ProfileScreen} options={renderHeaderOptions}/>
     </OptionsStack.Navigator>
   );
 };
--- src/Screens/MenuScreen.js
+++ src/Screens/MenuScreen.js
@@ -1,18 +1,28 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
 import CustomButton from "../Components/CustomUI/CustomButton";
-import { useDispatch } from 'react-redux';
+import { useDispatch } from "react-redux";
 import { Logout } from "../Store/Actions/auth";
 
-const MenuScreen = () => {
-    const dispatch = useDispatch();
-    return (
-        <View>
-          <CustomButton onPressHandler={() => {
-            dispatch(Logout());
-          }} title='Log out'/>
-        </View>
-    );
+const MenuScreen = ({navigation}) => {
+  const dispatch = useDispatch();
+  return (
+    <View>
+      <CustomButton
+        onPressHandler={() => {
+          navigation.navigate("Profile")
+        }}
+        title="Profile"
+      />
+
+      <CustomButton
+        onPressHandler={() => {
+          dispatch(Logout());
+        }}
+        title="Log out"
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({});
