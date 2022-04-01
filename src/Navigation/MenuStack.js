f51096613df585e34f7c41a0a6eb05f7a8354a74
KaremAridi <80266460+KaremAridi@users.noreply.github.com>
2022-04-01 12:05:00 +0300

    adding the coupon screen

--- src/Navigation/MenuStack.js
+++ src/Navigation/MenuStack.js
@@ -2,6 +2,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
 import MenuScreen from "../Screens/MenuScreen";
 import ProfileScreen from "../Screens/ProfileScreen"
+import CouponScreen from "../Screens/CouponScreen"
 import { renderHeaderOptions } from "../constants/CustomFts";
 
 const OptionsStack = createNativeStackNavigator();
@@ -11,6 +12,7 @@ const MenuStack = () => {
     <OptionsStack.Navigator>
       <OptionsStack.Screen name="MainMenu" component={MenuScreen} options={renderHeaderOptions}/>
       <OptionsStack.Screen name="Profile" component={ProfileScreen} options={renderHeaderOptions}/>
+      <OptionsStack.Screen name="Coupon" component={CouponScreen} options={renderHeaderOptions}/>
     </OptionsStack.Navigator>
   );
 };
---
+++ src/Screens/CouponScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const CouponScreen = () => {
+    return (
+        <View>
+            <Text>CouponScreen</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({});
+export default CouponScreen;
